49ba73ff768acc91542cc3b3bbd529cac31f23bf
duyphamluanh <anhduy123000@gmail.com>
2022-08-15 09:53:53 +0700

    updated

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,31 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/docusaurus-git-pages/blog',
+    component: ComponentCreator('/docusaurus-git-pages/blog', '7ea'),
+    exact: true
+  },
+  {
+    path: '/docusaurus-git-pages/blog/archive',
+    component: ComponentCreator('/docusaurus-git-pages/blog/archive', '877'),
+    exact: true
+  },
+  {
+    path: '/docusaurus-git-pages/blog/greetings',
+    component: ComponentCreator('/docusaurus-git-pages/blog/greetings', '302'),
+    exact: true
+  },
+  {
+    path: '/docusaurus-git-pages/blog/tags',
+    component: ComponentCreator('/docusaurus-git-pages/blog/tags', 'a31'),
+    exact: true
+  },
+  {
+    path: '/docusaurus-git-pages/blog/tags/greetings',
+    component: ComponentCreator('/docusaurus-git-pages/blog/tags/greetings', 'e92'),
+    exact: true
+  },
+  {
     path: '/docusaurus-git-pages/docs',
     component: ComponentCreator('/docusaurus-git-pages/docs', '2a2'),
     routes: [
---
+++ build/assets/js/d697708c.5aa4357c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus_git_pages=self.webpackChunkdocusaurus_git_pages||[]).push([[596],{4469:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
