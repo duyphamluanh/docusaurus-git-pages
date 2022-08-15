7aa3fb41f839823710e2a28796cca55f3625e094
duyphamluanh <anhduy123000@gmail.com>
2022-08-15 14:20:38 +0700

    add version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,6 +33,15 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          docs: {
+            lastVersion: 'current',
+            versions: {
+              current: {
+                label: '1.0.0',
+                path: '1.0.0',
+              },
+            },
+          },
         //   sidebarPath: require.resolve('./sidebars.js'),
         //   // Please change this to your repo.
         //   // Remove this to remove the "edit this page" links.
