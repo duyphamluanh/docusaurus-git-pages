f73dedd31f87bf9eeb2ae2d44e0c76ede416d8c4
duyphamluanh <anhduy123000@gmail.com>
2022-08-15 14:25:58 +0700

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,14 +32,14 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          lastVersion: 'current',
-          versions: {
-            current: {
-              label: '1.0.0',
-              path: '1.0.0',
-            },
-        },
+        // docs: {
+        //   lastVersion: 'current',
+        //   versions: {
+        //     current: {
+        //       label: '1.0.0',
+        //       path: '1.0.0',
+        //     },
+        // },
         //   sidebarPath: require.resolve('./sidebars.js'),
         //   // Please change this to your repo.
         //   // Remove this to remove the "edit this page" links.
@@ -52,7 +52,7 @@ const config = {
         //   // Remove this to remove the "edit this page" links.
         //   editUrl:
         //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        // },
         blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
